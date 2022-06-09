39e5cd5414ab8932b6c4f8f50882359f6f94f5bc
João Marcos <jmarcossferreira@hotmail.com>
2022-06-09 10:45:27 -0300

    Initial Commit

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  root: "src",
+  build: {
+    outDir: "../dist",
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["../.test/setup.js"],
+    include: ["**/*(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    exclude: ["node_modules", "dist", ".idea", ".git", ".cache"],
+  },
+});
